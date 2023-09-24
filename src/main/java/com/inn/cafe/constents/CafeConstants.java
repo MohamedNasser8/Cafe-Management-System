8b6d92aed76ef3c523f78df2a73917c8b0ad6b29
Mohamed Nasser <nassermohamed3222@gmail.com>
2023-09-24 18:47:07 +0300

    added

--- src/main/java/com/inn/cafe/constents/CafeConstants.java
+++ src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -3,7 +3,5 @@ package com.inn.cafe.constents;
 public class CafeConstants {
     public static final String SOMETHING_WENT_WRONG="Something Went Wrong.";
     public static final String INVALID_DATA="Invalid Data.";
-
     public static final String UNAUTHORIZED="Unauthorized access.";
-
 }
