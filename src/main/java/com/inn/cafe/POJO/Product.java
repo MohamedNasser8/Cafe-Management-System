95055b8a8deaaa007ff3d5ffb66818a64a08df2c
Mohamed Nasser <nassermohamed3222@gmail.com>
2023-09-27 22:49:03 +0300

    added product table

---
+++ src/main/java/com/inn/cafe/POJO/Product.java
@@ -0,0 +1,33 @@
+package com.inn.cafe.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "product")
+@DynamicInsert
+@DynamicUpdate
+public class Product implements Serializable {
+
+    public static final long serialVersionUid = 123456L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_fk", nullable = false)
+    private Category category;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private Integer price;
+    @Column(name = "status")
+    private String status;
+}
