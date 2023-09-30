158e7fa3110a2fcc8494f6cb36e089be24a455f3
Mohamed Nasser <nassermohamed3222@gmail.com>
2023-10-01 00:01:50 +0300

    front end structure
    add all needed components and routers

--- src/main/java/com/inn/cafe/serviceImpl/DashboardServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/DashboardServiceImpl.java
@@ -6,7 +6,6 @@ import com.inn.cafe.dao.ProductDao;
 import com.inn.cafe.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
